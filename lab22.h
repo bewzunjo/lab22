0b62e11064a2001a5beb97c2419bd2e21169b3c2
bewzunjo <saran_kim@cmu.ac.th>
2022-02-04 17:35:08 +0700

    lab22h

--- lab22.h
+++ lab22.h
@@ -64,10 +64,37 @@ void Unit::newTurn(){
 }
 
 
+bool Unit::isDead(){
+    if(hp <= 0) return true;
+    else return false;
+}
+void Unit::guard(){
+    guard_on = true;
+}
+int Unit::beAttacked(int oppatk){
+    int dmg;
+    if(!guard_on){
+    dmg = oppatk-def;
+    hp = hp-dmg;
+    return dmg;
+    }
+    else{
+    dmg = (oppatk-def)/3;
+    hp = hp-dmg;
+    return dmg;
+    }
+}
+int Unit::attack(Unit &target){
+    return target.beAttacked(atk);
+}
+int Unit::heal(){
+    int i = hpmax - hp;
+    int healhp =  rand()%21 + 10;
+    if(healhp >= i) healhp = i;
+    hp = hp + healhp;
+    return healhp;
+}
 
-/////////////////////////////////////////////////////////////////////////////////////
-//Write function members isDead(), guard(), heal(), beAttacked(), and attack() here//
-/////////////////////////////////////////////////////////////////////////////////////
 
 
 
